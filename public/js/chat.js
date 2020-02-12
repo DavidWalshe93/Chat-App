c6ed5b49fc6150e2739be81ffb497dbb9b921cda
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-02-12 21:47:31 +0000

    Added support for hitting enter key to send message to server from client.

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,13 +7,22 @@ socket.on("message", (msg) => {
     console.log(msg)
 });
 
-// Message transmitter
-document.querySelector("#button-addon2").addEventListener("click", () => {
+// Message transmitter on button press
+document.querySelector("#button-addon2").addEventListener("click", () => send());
 
+// Message transmitter on enter key
+document.querySelector("#msg-input").addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        console.log("hello");
+        send()
+    }
+});
+
+const send = () => {
     // Get input field.
     const input = document.querySelector("#msg-input");
     // Send message to server with text.
     socket.emit("sendMessage", input.value);
     // Clear input field.
-    input.value = ""
-});
+    input.value = "";
+};
