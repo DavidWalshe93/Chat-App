61adeb183eab8b72395830f28ee85662f47e0fe3
david.walshe93@gmail.com <b$P$JpQFY9cg>
2020-02-12 02:02:26 +0000

    Added some comments to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 // Created by David Walshe on 12/02/2020
 
+// included app.js and setup Express in doing so.
 const app = require("./app");
 
+// Setup port, done via env variable.
 const port = process.env.PORT;
 
+// Start Express application.
 app.listen(port, () => {
     console.log("Server is up on port " + port)
 });
